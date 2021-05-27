8b96be0526cce9c9fd041d75fa9d279e9b69f34b
Jérémy M <jeremy.maia@protonmail.com>
2021-05-27 14:55:50 +0200

    Fill GobanGraph properties

--- GobanGraph.java
+++ GobanGraph.java
@@ -2,5 +2,23 @@ import javax.swing.JPanel;
 
 public class GobanGraph extends JPanel
 {
-	
+	private SideLength sideLength;
+
+	public GobanGraph(SideLength sideLength)
+	{
+		this.setSideLength(sideLength);
+	}
+
+	public SideLength getSideLength()
+	{
+		return this.sideLength;
+	}
+
+	public void setSideLength(SideLength sideLength)
+	{
+		if (sideLength == null)
+			throw new NullPointerException();
+
+		this.sideLength = sideLength;
+	}
 }
