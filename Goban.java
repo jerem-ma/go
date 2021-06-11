146d0119af7bf6ea21c8f4595e52439a45d0191b
Jérémy M <jeremy.maia@protonmail.com>
2021-06-11 13:16:13 +0200

    Add isSuicide()

--- Goban.java
+++ Goban.java
@@ -76,7 +76,12 @@ public class Goban
 	public boolean isLegal(StoneInfo stone)
 	{
 		// Fill this method to tell if a move is legal
-		return this.getNumberOfGroupLiberties(stone) > 0;
+		return !isSuicide(stone);
+	}
+
+	private boolean isSuicide(StoneInfo stone)
+	{
+		return this.getNumberOfGroupLiberties(stone) == 0;
 	}
 
 	public void play(StoneInfo stone)
