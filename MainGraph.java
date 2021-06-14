6de744c5bc0eee4b4c934846521c0d8a89ae571a
Jérémy M <jeremy.maia@protonmail.com>
2021-06-14 16:04:54 +0200

    Create MainGraph base

---
+++ MainGraph.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainGraph extends JPanel
+{
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		JButton play = new JButton("Play");
+		play.addActionListener(this::playClicked);
+		this.add(play);
+
+		JButton options = new JButton("Options");
+		options.addActionListener(this::optionsClicked);
+		this.add(options);
+
+		JButton quit = new JButton("Quit");
+		quit.addActionListener(this::quitClicked);
+		this.add(quit);
+	}
+
+	private void playClicked(ActionEvent e)
+	{
+
+	}
+
+	private void optionsClicked(ActionEvent e)
+	{
+
+	}
+
+	private void quitClicked(ActionEvent e)
+	{
+
+	}
+}
